83d92480fbb987e8aa1a9aaabb1c62e8c550728d
Zalimkhan <51163032+Zalimkhan99@users.noreply.github.com>
2020-08-17 11:39:15 +0300

    /finish progect

--- index.js
+++ index.js
@@ -8,60 +8,74 @@ var url = 'http://localhost/InfoBase/hs/Demo/List/'
 var phone ='';
 var promise;
 var auth = false;
-/*
-var url = 'http://localhost/InfoBase/hs/Demo/List/';
-var phone = '89286748487'
-var promise =  fetch(url +phone ,    { method: 'GET' } )
-.then( promise => promise.json() )
-.then( json => console.log(json) )
-.catch( error => console.error('Ввели не правлиьный номеп') );
-console.log(promise);
+var newUrl='';
+var data = '';
 
-*/
+
 let promiseStart = new Promise((resove, reject)=>{
     bot.onText(/\/start/, msg=>{
         if (phone !="/start")
-        bot.sendMessage(msg.chat.id, "Добро пожаловать! Введите номер телефона:");
-        if (phone =="/start"){
-            throw  bot.sendMessage(msg.chat.id , 'команда уже введена') 
-        }
+        bot.sendMessage(msg.chat.id, "Добро пожаловать! Подтвердите свою личность /auth ");
+        
+        
+        
 
     })
 })
 
 
-
 let pormisePhone = new Promise((resove, reject)=>{
-    bot.onText(/\/start/, msg=>{
-        //проверка на коректность введеного номера
-        if(phone==''&& phone != '/start'){ 
+    bot.onText(/\/auth/, msg=>{
+        bot.sendMessage(msg.chat.id, "Введите номер телефона:");
             //колбек берет данные с базы данных
             bot.on('message', msg=>{
-                phone = msg.text
-                url = 'http://localhost/InfoBase/hs/Demo/List/'+phone
-                    //добавил условие чтоб не отправлялись подряд сообщения при отправки start
-                if (phone !="/start"){
-                 promise =  fetch(url ,    { method: 'GET' } )
-                .then( promise => promise.json() )
+               
+                phone = msg.text;
+                if (phone.match(/^\d+$/) ){
+                    url = 'http://localhost/InfoBase/hs/Demo/List/'+phone;
+                    newUrl = url;
+                 promise =  fetch(newUrl ,    { method: 'GET' } )
+                .then( promise => promise.json()  )
                 .then( json => {
                     auth = true;
-                    console.log(auth)
-                    bot.sendMessage(msg.chat.id ,'номер есть в базе')
+                    data =  JSON.stringify(json,null,2);
+                    bot.sendMessage(msg.chat.id ,'Чтоб узнать балнас введите /info ' )
+                    
                 } )
+              
                 
                .catch( error =>{
-                   bot.sendMessage(msg.chat.id , 'Ввели не правлиьный номер, повторите попытку') 
-                   auth = false;
-                   console.log(auth)
+                   if(auth == true){
+                       console.log(auth)
+                   throw bot.sendMessage(msg.chat.id , 'некорректная команда') 
+                   }
+                  
+                   
+                   else{
+                    auth = false;
+                       
+                    throw bot.sendMessage(msg.chat.id , 'вас нет в базе') 
+                   
+                   }
             });
                 }
-
-            })//конец колбека on message
-        } //конец условия phone==''&& phone != '/start'     
+                
+                
+            })//конец колбека on message          
     }) //конец колбека onText(/start)
 }) //конец promisePhone
 
-let promiseKeybord = new Promise((reseove, reject)=>{
-
+let pormiseInfo = new Promise((resove, reject)=>{
     
+    bot.onText(/\/info/, msg=>{
+        if (data!=''){
+        bot.sendMessage(msg.chat.id, data)
+        }
+        else bot.sendMessage(msg.chat.id, 'вы не авторизованы!')
+
+    })
+
+
 })
+
+
