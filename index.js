e97b11f493aa2ac28666aa810bc1018d7e39ce78
Zalimkhan99 <zalik-1999@mail.ru>
2020-08-25 11:05:58 +0300

    test

--- index.js
+++ index.js
@@ -2,57 +2,77 @@ process.env.NTBA_FIX_319 = 1;
 //подключение модулей
 const fetch = require("node-fetch");
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1356849321:AAGYRYMGBGnnOzJCubrm2B3reK2qWZNXXV8';
-const bot = new TelegramBot(token, {polling: true});
-var url = 'http://localhost/InfoBase/hs/Demo/List/'
-var phone ='';
-var promise;
+const await = require('await')
+const token = '1356849321:AAGYRYMGBGnnOzJCubrm2B3reK2qWZNXXV8'; // токен бота телеграм
+const bot = new TelegramBot(token, {
+	polling: true
+});
+//объявление глобальных переменных
+var url = ''
+var phone = '';
+var newUrl = '';
+var dataJSON = '';
+var response = '';
 var auth = false;
-
-let promiseStart = new Promise((resove, reject)=>{
-    bot.onText(/\/start/, msg=>{
-        if (phone !="/start")
-        bot.sendMessage(msg.chat.id, "Добро пожаловать! Введите номер телефона:");
-        if (phone =="/start"){
-            throw  bot.sendMessage(msg.chat.id , 'команда уже введена') 
-        }
-
-    })
-})
-
-
-
-let pormisePhone = new Promise((resove, reject)=>{
-    bot.onText(/\/start/, msg=>{
-        //проверка на коректность введеного номера
-        if(phone==''&& phone != '/start'){ 
-            //колбек берет данные с базы данных
-            bot.on('message', msg=>{
-                phone = msg.text
-                url = 'http://localhost/InfoBase/hs/Demo/List/'+phone
-                    //добавил условие чтоб не отправлялись подряд сообщения при отправки start
-                if (phone !="/start"){
-                 promise =  fetch(url ,    { method: 'GET' } )
-                .then( promise => promise.json() )
-                .then( json => {
-                    auth = true;
-                    console.log(auth)
-                    bot.sendMessage(msg.chat.id ,'номер есть в базе')
-                } )
-                
-               .catch( error =>{
-                   bot.sendMessage(msg.chat.id , 'Ввели не правлиьный номер, повторите попытку') 
-                   auth = false;
-                   console.log(auth)
-            });
-                }
-
-            })//конец колбека on message
-        } //конец условия phone==''&& phone != '/start'     
-    }) //конец колбека onText(/start)
-}) //конец promisePhone
-
-let promiseKeybord = new Promise((reseove, reject)=>{
-
-    
+var sendMs= 'https://api.telegram.org/bot1356849321:AAGYRYMGBGnnOzJCubrm2B3reK2qWZNXXV8/sendMessage?chat_id=315069916&text= работает ';
+//создание промиса 
+var promiseTelegramBot = new Promise((resove, regect) => {
+	setTimeout(() => resove(1), 1000);
+});
+//команда start
+promiseTelegramBot.then(function(resove) {
+		return resove = bot.onText(/\/start/, msg => {
+			bot.sendMessage(msg.chat.id, "Добро пожаловать, "+ msg.chat.first_name +" для авторизации введите команду /auth ");
+		})
+	})
+	// команда auth
+promiseTelegramBot.then(function(resove) {
+		return resove = bot.onText(/\/auth/, msg => {
+				let text = '';
+				if (auth == true) {
+					text = 'Вы авторизованы'
+				} else text = 'Введите номер телефона:'
+				bot.sendMessage(msg.chat.id, text);
+			})
+			//ввод номера телефона
+	}).then(bot.on('message', msg => {
+			phone = msg.text;
+			if (phone.match(/^\d+$/)) {
+				url = 'http://localhost/DemoRetail1/hs/tlg//List/' + phone;
+				//url = 'http://localhost/InfoBase/hs/Demo/List/' + phone;
+				newUrl = url;
+				dataReq()
+				async function dataReq() {
+					response = await fetch(newUrl);
+					if (response.ok) {
+						let json = await response.text();
+						dataJSON = json;
+						auth = true;
+						return bot.sendMessage(msg.chat.id, 'Чтоб узнать сколько осталось бонусов на вашей дисконтной карте, введите команду /info');
+					} else if (!response.ok && auth == false) {
+						bot.sendMessage(msg.chat.id, 'Номер введен не правильно или не зарегестрирован');
+					} else bot.sendMessage(msg.chat.id, 'вы авторизованы');
+				} //конец асинхронной функции
+			} // конец if
+		}) //конец колбека on message  
+	)
+	// команда info
+promiseTelegramBot.then(function(resove) {
+		return resove = bot.onText(/\/info/, msg => {
+			if (dataJSON != '') {
+				bot.sendMessage(msg.chat.id, dataJSON)
+			} else bot.sendMessage(msg.chat.id, 'вы не авторизованы!')
+		})
+	})
+	//команда exit
+promiseTelegramBot.then(function(resove) {
+	return resove = bot.onText(/\/exit/, msg => {
+		auth = false;
+		phone = '';
+		url='';
+		newUrl = '';
+		dataJSON = '';
+		response = '';
+		bot.sendMessage(msg.chat.id, 'Всего доброго!')
+	})
 })
